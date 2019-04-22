867b9aed004d2b9844e93251135ae773537defb1
mediaios <iosmediadev@gmail.com>
2019-04-22 16:04:41 +0800

    hook NSArray(NSMutableArray,NSArray)

--- MISafeApp/MISafeApp/container/NSArray+MiSafe.m
+++ MISafeApp/MISafeApp/container/NSArray+MiSafe.m
@@ -16,15 +16,8 @@
 {
     static dispatch_once_t miOnceToken;
     dispatch_once(&miOnceToken, ^{
-        
-        [NSObject miSwizzleInstanceMethod:objc_getClass("__NSPlaceholderArray") swizzSel:@selector(initWithObjects:count:) toSwizzledSel:@selector(miInitWithObjects:count:)];
-        
         /********** 构造数组 **********/
-        [NSObject miSwizzleInstanceMethod:objc_getClass("__NSArrayI")
-                                 swizzSel:@selector(objectAtIndexedSubscript:)
-                            toSwizzledSel:@selector(miArrObjectAtIndexSubscript:)];
-        
-        
+        [NSObject miSwizzleInstanceMethod:objc_getClass("__NSPlaceholderArray") swizzSel:@selector(initWithObjects:count:) toSwizzledSel:@selector(miInitWithObjects:count:)];
         
         /********** 取元素 **********/
         // [array objectAtIndex:1000] 方式发生的crash
@@ -39,7 +32,45 @@
                                  swizzSel:@selector(objectAtIndexedSubscript:)
                             toSwizzledSel:@selector(miMutaArrObjectAtIndexSubscript:)];
         
-        [NSObject miSwizzleInstanceMethod:objc_getClass("__NSArrayM") swizzSel:@selector(insertObject:atIndex:) toSwizzledSel:@selector(miMutaInsertObject:atIndex:)];
+        /********** 插入元素 **********/
+        // 插入元素： 元素为nil; index非法
+        [NSObject miSwizzleInstanceMethod:objc_getClass("__NSArrayM")
+                                 swizzSel:@selector(insertObject:atIndex:)
+                            toSwizzledSel:@selector(miMutaInsertObject:atIndex:)];
+        
+        /********** 删除元素 **********/
+        [NSObject miSwizzleInstanceMethod:objc_getClass("__NSArrayM")
+                                 swizzSel:@selector(removeObjectsInRange:)
+                            toSwizzledSel:@selector(miRemoveObjectsInRange:)];
+        
+        /********** 替换数组中元素 **********/
+        [NSObject miSwizzleInstanceMethod:objc_getClass("__NSArrayM")
+                                 swizzSel:@selector(replaceObjectAtIndex:withObject:)
+                            toSwizzledSel:@selector(miReplaceObjectAtIndex:withObject:)];
+        
+        
+        /********** 交换数组中元素 **********/
+        [NSObject miSwizzleInstanceMethod:objc_getClass("__NSArrayM")
+                                 swizzSel:@selector(exchangeObjectAtIndex:withObjectAtIndex:)
+                            toSwizzledSel:@selector(miExchangeObjectAtIndex:withObjectAtIndex:)];
+        
+        /********** 删除元素 **********/
+        [NSObject miSwizzleInstanceMethod:objc_getClass("NSMutableArray")
+                                 swizzSel:@selector(removeObject:inRange:)
+                            toSwizzledSel:@selector(miRemoveObject:inRange:)];
+        [NSObject miSwizzleInstanceMethod:objc_getClass("NSMutableArray")
+                                 swizzSel:@selector(removeObjectIdenticalTo:inRange:)
+                            toSwizzledSel:@selector(miRemoveObjectIdenticalTo:inRange:)];
+        
+        /********** 替换元素 **********/
+        [NSObject miSwizzleInstanceMethod:objc_getClass("NSMutableArray")
+                                 swizzSel:@selector(replaceObjectsInRange:withObjectsFromArray:range:)
+                            toSwizzledSel:@selector(miReplaceObjectsInRange:withObjectsFromArray:range:)];
+        [NSObject miSwizzleInstanceMethod:objc_getClass("NSMutableArray")
+                                 swizzSel:@selector(replaceObjectsInRange:withObjectsFromArray:)
+                            toSwizzledSel:@selector(miReplaceObjectsInRange:withObjectsFromArray:)];
+        
+        
         [NSObject miSwizzleInstanceMethod:objc_getClass("__NSArrayM") swizzSel:@selector(integerValue) toSwizzledSel:@selector(miIntegerValue)];
     });
 }
@@ -48,6 +79,7 @@
 - (id)miInitWithObjects:(const id _Nonnull __unsafe_unretained *)objects
                   count:(NSUInteger)cnt
 {
+    
     BOOL hasNilObject = NO;
     for (NSUInteger i = 0; i < cnt; i++) {
         if ([objects[i] isKindOfClass:[NSArray class]]) {
@@ -110,10 +142,63 @@
     return [self miMutaArrObjectAtIndex:index];
 }
 
+#pragma mark - 插入元素
 - (void)miMutaInsertObject:(id)object atIndex:(NSUInteger)index
 {
-    if (object) {
+    if (object && index <= self.count) {
         return [self miMutaInsertObject:object atIndex:index];
+    }
+}
+
+#pragma mark - 删除元素
+- (void)miRemoveObjectsInRange:(NSRange)range
+{
+    if ((NSMaxRange(range) - 1) < self.count) {
+        [self miRemoveObjectsInRange:range];
+    }
+}
+
+- (void)miRemoveObject:(id)anObject inRange:(NSRange)range
+{
+    if (anObject && (NSMaxRange(range) - 1) < self.count) {
+        [self miRemoveObject:anObject inRange:range];
+    }
+}
+
+- (void)miRemoveObjectIdenticalTo:(id)anObject inRange:(NSRange)range
+{
+    if (anObject && (NSMaxRange(range) - 1) < self.count) {
+        [self miRemoveObjectIdenticalTo:anObject inRange:range];
+    }
+}
+
+#pragma mark - 替换元素
+- (void)miReplaceObjectAtIndex:(NSUInteger)index withObject:(id)anObject
+{
+    if (index < self.count && anObject) {
+        [self miReplaceObjectAtIndex:index withObject:anObject];
+    }
+}
+
+- (void)miReplaceObjectsInRange:(NSRange)range withObjectsFromArray:(id)otherArray range:(NSRange)otherRange
+{
+    if ((NSMaxRange(range) -1 < self.count) && [otherArray isKindOfClass:[NSArray class]] && (NSMaxRange(otherRange)-1 < ((NSArray *)otherArray).count) ) {
+        [self miReplaceObjectsInRange:range withObjectsFromArray:otherArray range:otherRange];
+    }
+}
+
+- (void)miReplaceObjectsInRange:(NSRange)range withObjectsFromArray:(id)otherArray
+{
+    if (NSMaxRange(range) -1 < self.count) {
+        [self miReplaceObjectsInRange:range withObjectsFromArray:otherArray];
+    }
+}
+
+#pragma mark - 交换元素
+- (void)miExchangeObjectAtIndex:(NSUInteger)idx1 withObjectAtIndex:(NSUInteger)idx2
+{
+    if (idx1 < self.count && idx2 < self.count) {
+        [self miExchangeObjectAtIndex:idx1 withObjectAtIndex:idx2];
     }
 }
 
--- demo/MISafeAppDemo/MISafeAppDemo/ViewController.m
+++ demo/MISafeAppDemo/MISafeAppDemo/ViewController.m
@@ -18,17 +18,124 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    [self test_array_construct];
+    [self test_mutableArray];
 }
 
-- (void)test_array_construct
+- (void)test_array
 {
-    NSMutableArray *mutaArray = [[NSMutableArray alloc] init];
+//    NSLog(@"%s",__func__);
     NSString *a = nil;
-    [mutaArray addObject:a];
+//    NSArray *array = @[@"a1",a];
+//    NSLog(@"app: %@",array);
     
-    NSArray *array = @[@"a1",a];
-    NSLog(@"app: %@",array);
+
+    
+    NSArray *array1 = nil;
+    NSArray *array2 = [[NSArray alloc] initWithArray:array1];
+    NSLog(@"array2: %@",array2);
+
+    array2 = [[NSArray alloc] initWithObjects:@"a",a];
+    NSLog(@"array2: %@",array2);
+
+    NSArray *array3 = [NSArray arrayWithArray:array1];
+    NSLog(@"array3: %@",array3);
+
+    NSArray *array4 = [NSArray arrayWithObjects:@"a",a, nil];
+    NSLog(@"array=4: %@",array4);
+
+    NSString *str[3];
+    str[0] = @"F";
+    str[1] = @"S";
+    str[2] = a;
+    NSArray *array5 = [NSArray arrayWithObjects:str count:3];
+    NSLog(@"array=5: %@",array5);
+
+    NSArray *array6 = [NSArray arrayWithObject:a];
+    NSLog(@"array6: %@",array6);
+
+    NSArray *array7 = [[NSArray alloc] initWithArray:array1 copyItems:YES];
+    NSLog(@"array7: %@",array7);
+    
+}
+
+- (void)test_mutableArray
+{
+    NSString *a = nil;
+    
+    NSMutableArray *mutaArray1 = [[NSMutableArray alloc] init];
+    [mutaArray1 addObject:a];
+    NSLog(@"添加空元素，mutaArray1: %@",mutaArray1);
+    
+    NSMutableArray *muta = nil;
+    [mutaArray1 addObjectsFromArray:muta];
+    NSLog(@"添加空数组, mutaArray1: %@",mutaArray1);
+    
+    // 插入
+    [mutaArray1 insertObject:a atIndex:0];
+    NSLog(@"插nil元素后 mutaArray1: %@",mutaArray1);
+    
+    // 插入
+    [mutaArray1 insertObject:@"a" atIndex:20];
+    NSLog(@"插入合法元素，index非法时 mutaArray1: %@",mutaArray1);
+    
+    // 删除
+    [mutaArray1 removeObjectAtIndex:20];
+    NSLog(@"删除元素： mutaArray1: %@",mutaArray1);
+    
+    [mutaArray1 addObjectsFromArray:@[@1,@2,@3]];
+    [mutaArray1 removeObject:a inRange:NSMakeRange(10, 2)];
+    NSLog(@"删除range元素: mutaArray1: %@",mutaArray1);
+    
+    [mutaArray1 removeObject:@"5"];
+    NSLog(@"删除元素-removeObject--mutaArray1: %@",mutaArray1);
+    
+    
+    [mutaArray1 removeObjectsInArray:muta];
+    NSLog(@"删除元素--removeObjectsInArray:-----mutaArray1: %@",mutaArray1);
+    
+    [mutaArray1 removeObjectsInRange:NSMakeRange(10, 20)];
+    NSLog(@"删除元素--removeObjectsInRange:-----mutaArray1: %@",mutaArray1);
+    
+    [mutaArray1 removeObjectIdenticalTo:a];
+    NSLog(@"删除元素-removeObjectIdenticalTo:---mutaArray1: %@",mutaArray1);
+    
+    [mutaArray1 removeObjectIdenticalTo:@2 inRange:NSMakeRange(0, 20)];
+    NSLog(@"删除元素-removeObjectIdenticalTo:inRange:---mutaArray1: %@",mutaArray1);
+    
+    [mutaArray1 replaceObjectAtIndex:20 withObject:@"a"];
+    NSLog(@"下表非法，替换元素： mutaArray1: %@",mutaArray1);
+    
+    [mutaArray1 replaceObjectsInRange:NSMakeRange(0, 10) withObjectsFromArray:muta];
+    NSLog(@"替换元素--replaceObjectsInRange: withObjectsFromArray:---mutaArray1: %@",mutaArray1);
+    
+    NSArray *muta2 = @[@"5",@"6",@"7"];
+    [mutaArray1 replaceObjectsInRange:NSMakeRange(0, 1) withObjectsFromArray:muta range:NSMakeRange(0, 3)];
+    NSLog(@"替换元素--replaceObjectsInRange: withObjectsFromArray:range:---mutaArray1: %@",mutaArray1);
+    
+    // set array
+    [mutaArray1 setArray:muta];
+    NSLog(@"添加元素--setArray:--mutaArray1: %@",mutaArray1);
+    
+    
+    
+    [mutaArray1 addObject:@"a"];
+    [mutaArray1 replaceObjectAtIndex:0 withObject:a];
+    NSLog(@"元素为空，替换元素： mutaArray1: %@",mutaArray1);
+    
+    [mutaArray1 exchangeObjectAtIndex:20 withObjectAtIndex:21];
+    NSLog(@"交换元素后： %@",mutaArray1);
+    
+    
+}
+
+- (void)test_array_insert
+{
+    /*** 构造数组 ***/
+    NSMutableArray *mutaArray = [[NSMutableArray alloc] init];
+    [mutaArray addObject:@"0"];
+    NSString *a  = nil;
+    [mutaArray insertObject:a atIndex:1];
+    NSLog(@"mutaArray: %@",mutaArray);
     
 }
 
